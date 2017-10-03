90bf26a6399d12b43d66a8605f9b7995458f7b5a
Amber Rucker <amber.r.rucker@gmail.com>
2017-10-02 23:15:21 -0500

    feat(global): hello world

---
+++ understrap-child/front-page.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * The template for displaying the front page.
+ *
+ * This is the template that displays on the front page only.
+ *
+ * @package _dandyland
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<header class="header-container">
+			<div class="header-logo">
+			</div>
+		</header>
+		
+		<section class="about-wrapper">
+			<div class="about-container">
+				<div class="about-row">
+					<div class="about-logoWrapper">
+						<div class="about-logoImg">&nbsp;</div>
+					</div>
+				</div>
+				<div class="about-row">
+					<div class="about-textWrapper">
+						<p class="about-tagline">Established in 1996, Dandyland Custom Tattoo and Professional Body Piercing has been adorning the San Antonio area and people worldwide with award winning tattoo art and high precision, high quality body piercings for over 18 years.</p>
+					</div>
+				</div>
+				<div class="about-row">
+					<div class="about-textWrapper">
+						<h2 class="about-title">About</h2>
+						<p class="about-desc">	Dandyland has been a namesake in not only the San Antonio tattooing and body piercing industry, but also the international body modification community as well. Throughout the years the same philosophy that we first opened our doors with still carries forth: Provide body art aficionados with the highest quality tattoos, piercings, and body jewelry possible in a safe, clean, positive environment. Keeping true to this philosophy has continued to make Dandyland San Antonio’s premier choice for body art, as evidenced by Dandyland having been voted the #1 tattoo and piercing studio in the San Antonio Current’s Readers Poll 14 of the last 16 years.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="split-container">
+		  <div class="split-item cat-tattooWrapper">
+		    <div class="cat-tattooContainer">
+			  <h2 class="cat-tattooTitle">Tattoos</h2>
+			  <p class="cat-tattooText">Our dedicated, award-winning artists are here to create your unique, custom tattoos. The artists work primarily on appointment basis, but are more than happy to take walk-ins if scheduling allows.</p>
+			  <div class="cat-tattooButtonContainer button-container">
+			  	<button class="cat-tattooButton button">View Upcoming Guest Artists</button>
+			  </div>
+		    </div>
+		
+		  </div>
+		  <div class="split-item cat-piercingWrapper">
+		    <div class="cat-piercingContainer">
+			  <h2 class="cat-piercingTitle">Piercings</h2>
+			  <p class="cat-piercingText">Our piercers are the only members of the Association of Professional Piercers in San Antonio. We use nothing but the highest quality, implant grade , internally threaded jewelry. Our piercers annually attend the APP conference in Vegas to continue their education so they can keep up to date with industry standards.</p>
+			  <p class="cat-piercingText">If we don’t have exactly what you are looking for in stock, we have no problems getting it special ordered for you from our extensive list of reputable jewelry manufacturers. Our piercers are also very knowledgable in jewelry design, precious metals and gemstones, and aesthetics and can aid you in designing and custom ordering your dream body jewelry.</p>
+			  <div class="cat-piercingButtonContainer button-container">
+			  	<button class="cat-piercingButton button">View Jewelry Selection</button>
+			  </div>
+		    </div>
+		  </div>
+		</div>	
+		
+			
+		<section class="artists-wrapper">
+			<div class="artists-container">
+				<div class="artists-row">
+					<div class="artist-contentWrapper">
+						<h2 class="artists-title">Tattoo Artists</h2>
+						<div class="artists-galleryWrapper">
+						</div>
+					</div>
+				</div>
+				<div class="artists-row">
+					<div class="artist-contentWrapper">
+						<h2 class="artists-title">Piercers</h2>
+						<div class="artists-galleryWrapper">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section class="gallery-wrapper">
+			<div class="gallery-container">
+				<div class="gallery-row">
+					<div class="gallery-contentWrapper">
+						<h2 class="gallery-title">Photostream</h2>
+						<div class="gallery-galleryWrapper">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<div class="split-container">
+		  <div class="split-item contact-mapWrapper">
+		    <div class="contact-mapImage">
+			  &nbsp;
+		    </div>
+		
+		  </div>
+		  <div class="split-item contact-formWrapper">
+		    <div class="contact-formText">
+			  <h2 class="contact-formTitle">Contact</h2>
+			  <p>Email: dandylandtattoo@sbcglobal.net</p>
+			  <p>Phone: (210) 432-5747</p>
+			  <p>Address: 1620 Fredericksburg Rd<br/>
+San Antonio, TX 78201</p>
+		    </div>
+		  </div>
+		</div>
+
+	</div><!-- #primary -->
+
+
+<?php
+get_footer();
+
